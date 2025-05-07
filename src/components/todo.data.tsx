d2eb77f6a079865112c3c152bfee9ce3ea8a1098
HP_Nam <namphg1412@gmail.com>
2025-05-07 11:08:40 +0700

    passing props and defining type

---
+++ src/components/todo.data.tsx
@@ -0,0 +1,44 @@
+
+// [1, 3, 4] : number[]
+
+// interface definition ~ same as type definition
+interface IProps {
+    todos: {
+        id: number;
+        title: string;
+        isComplete: boolean;
+    }[];
+    owner: string;
+    age: number;
+    isDeveloper: boolean;
+}
+
+// type definition
+type TProps = {
+    todos: {
+        id: number;
+        title: string;
+        isComplete: boolean;
+    }[];
+    owner: string;
+    age: number;
+    isDeveloper: boolean;
+}
+
+const TodoData = (props: IProps) => {
+    const { todos, owner } = props;
+    return (
+        <div>
+            <div>Owner: {owner}</div>
+            {todos.map(item => {
+                return (
+                    <div key={item.id}>
+                        <div>{item.title}</div>
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+
+export default TodoData;
--- src/components/todo.list.tsx
+++ src/components/todo.list.tsx
@@ -1,15 +1,38 @@
+import TodoData from "./todo.data";
 
 
 const TodoList = () => {
+
+    const todos = [
+        {
+            id: 1,
+            title: "Learn React TypeScript",
+            isComplete: false
+        },
+        {
+            id: 2,
+            title: "Watch anime",
+            isComplete: true
+        },
+        {
+            id: 3,
+            title: "Read manga",
+            isComplete: true
+        },
+    ]
+
     return (
-        <div>todo list</div>
+        <div>
+            <div>My todo list</div>
+            <br />
+            <TodoData
+                todos={todos}
+                owner={"Nam Hoang"}
+                age={20}
+                isDeveloper={true}
+            />
+        </div>
     )
 }
 
 export default TodoList;
-
-// const TodoList: React.FunctionComponent = () => (
-//     <div>TodoList Function component</div>
-// );
-
-// export default TodoList;
